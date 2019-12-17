060ddb57e8205bdb9e159a8b43f0b6e643fc4ecc
Evgeniy Bogachov <bogachov.e@icloud.com>
2019-12-17 09:26:58 +0800

    Init

---
+++ SwiftUIHomework/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIHomework
+//
+//  Created by Evgeniy Bogachov on 17.12.2019.
+//  Copyright © 2019 Evgeniy Bogachov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
